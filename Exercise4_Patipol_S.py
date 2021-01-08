cc68f3516897579f6fd3487fd79bfe7cb1dadc79
patipols <s.patipol@gmail.com>
2021-01-09 00:12:05 +0700

    Create Exercise4_Patipol_S.py

---
+++ Exercise4_Patipol_S.py
@@ -0,0 +1,12 @@
+eng = 60.5
+biz = 80.0
+cs = 75.0
+program = 95.5
+
+print('----- Your Score -----')
+print('')
+print('Foundation English               : ',eng)
+print('General Business                 : ',biz)
+print('Introduction to Computer Systems : ',cs)
+print('Computer Programming             : ',program)
+
