ee366ba3da8483f6321492ca4c14491e9caf9347
patipols <s.patipol@gmail.com>
2021-01-08 23:58:29 +0700

    Update Exercise5_2_Patipol_S.py

--- Exercise5_2_Patipol_S.py
+++ Exercise5_2_Patipol_S.py
@@ -1,5 +1,30 @@
-s = int(input("Please insert Distance : "))
-t = int(input("Please insert time : "))
+username = input("Username :")
+password = input("Password :")
 
-v = s/t 
-print(int(v),"km/h")
+if username == 'admin' and password == '1234':
+  print('------Welcome to Max Shop------')
+  print('------1. Coffee 10 THB-------')
+  print('------2. Banana  8 THB-------')
+  print('------3. Water   6 THB-------')
+  selectValue = int(input("Select Product >>>"))
+  
+  priceCoffee = 10
+  priceBanana = 8
+  priceWater = 6
+
+  demand = int(input("-----Number of Qty :"))
+  
+  if selectValue == 1:
+    print("Number of Coffee :",demand)
+    print("Total Amount: ",demand * priceCoffee)
+  elif selectValue == 2:
+    print("Number of Banana :",demand)
+    print("Total Amount: ",demand * priceBanana)
+  elif selectValue == 3:
+    print("Number of Water :",demand)
+    print("Total Amount: ",demand * priceWater)
+  else:
+    print("Not have product selected")
+    
+else:
+  print("Username or Password incorrect!!!!")
