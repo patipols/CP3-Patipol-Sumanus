fac97167aec948ab0bbc0f5bf7cca9630e7e2690
patipols <s.patipol@gmail.com>
2021-01-10 00:47:54 +0700

    Create Lecture50.py

---
+++ Lecture50.py
@@ -0,0 +1,16 @@
+def add_number(x,y):
+  print(x+y)
+
+def substract_number(x,y):
+  print(x-y)
+
+def multiple_number(x,y):
+  print(x*y)     
+
+def divide_number(x,y):
+  print(x/y)   
+
+add_number(10,10)  
+substract_number(2,1)
+multiple_number(5,4)
+divide_number(10,4)
