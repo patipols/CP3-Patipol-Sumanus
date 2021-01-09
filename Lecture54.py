1c8f2072646f1fe5d2d16c5c2b53d346b2a4fdd7
patipols <s.patipol@gmail.com>
2021-01-10 01:47:07 +0700

    Update Lecture54.py

--- Lecture54.py
+++ Lecture54.py
@@ -1,6 +1,40 @@
-def vatCal(amount):
-  totalAmount = amount + (amount * (7/100))
-  return totalAmount
+def login(username,password):
+  while username != 'admin' or password != '1234':
+    print('Wrong')
+    username = input('Username: ')
+    password = input('Password: ')
 
-totalAmount = float(input('Input Total Amount: '))
-print(vatCal(totalAmount))  
+def showMenu():
+  print('----------iShop------------')
+  print('1. Vat Calculator')
+  print('2. Price Calculator')
+
+def selector():
+  option = input('Choose Selector: ')
+  return option     
+
+def vatCal():
+  print('----------1. Vat Calculator----------------')
+  totalAmount = float(input('Input Total Amount: '))  
+  totalAmount = totalAmount + (totalAmount * (7/100))
+  print(totalAmount)
+
+def ProdCal():
+  print('-----------2. Price Calculator--------------')
+  priceProduct1 = float(input('Input Price of Product 1: '))  
+  priceProduct2 = float(input('Input Price of Product 2: '))  
+  totalAmount = priceProduct1 + priceProduct2
+  print(totalAmount) 
+
+username = input('Username: ')
+password = input('Password: ')
+
+login(username,password)
+showMenu()
+option = selector()
+if option == '1':
+  vatCal()
+elif option == '2':
+  ProdCal()
+else:
+  print("Not Correct")
